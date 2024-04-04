cb57c4816a5cd47515d01b8ec29f520db1cc3c49
Morten la Cour <lacour@gmail.com>
2024-04-04 10:38:41 +0200

    use sp instead of key

--- Code/translatedemo/Program.cs
+++ Code/translatedemo/Program.cs
@@ -1,8 +1,9 @@
 ﻿using Azure;
 using Azure.AI.Translation.Text;
+using Azure.Identity;
 
 
-string key = "d7627....";
+//string key = "d76276d90b774b71a5482d1c29ec8c81";
 string endpoint = "https://ai102mlcservice1.cognitiveservices.azure.com/";
 
 string languages = "da,pl";
@@ -11,7 +12,20 @@ string content = args[0];
 
 string[] languageArray = languages.Split(',');
 
-var credential = new AzureKeyCredential(key);
+//var credential = new AzureKeyCredential(key);
+var clicredential = new AzureCliCredential();
+var spcredential = new ClientSecretCredential(
+        clientId: "7d59f29f-58f9-4ae3-822f-4c1ce61a9367",
+        tenantId: "551c586d-a82d-4526-b186-d061ceaa589e",
+        clientSecret: "MMW....");
+
+var credential = new ChainedTokenCredential(
+        clicredential,
+        spcredential);
+
+
+//var credential = new DefaultAzureCredential();
+
 
 
 var client = new TextTranslationClient(
